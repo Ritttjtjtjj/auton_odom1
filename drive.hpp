a2510c0cee007032bbeded31e16efdbb8e225176
Ritttjtjtjj <sudhaskrr@gmail.com>
2025-12-12 11:56:47 -0800

    Create drive.hpp

---
+++ drive.hpp
@@ -0,0 +1,13 @@
+#ifndef DRIVE_HPP
+#define DRIVE_HPP
+
+class Drive {
+public:
+    Drive() = default;
+
+    // left/right in [-127,127]
+    void setPower(double left, double right);
+    void stop();
+};
+
+#endif // DRIVE_HPP
